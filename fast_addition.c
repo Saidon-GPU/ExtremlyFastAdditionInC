51aedad8ed407552e5e09f6367a868187740aa60
Saidon-GPU <Primary.atias2@gmail.com>
2025-03-17 17:19:35 +0200

    Update fast_addition.c

--- fast_addition.c
+++ fast_addition.c
@@ -1,1 +1,18 @@
+#include <fast_addition.h>
 
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SGN(x) ((x) < 0 ? -1 : 1)
+
+int fast_addition(int a, int b) {
+  const int l = 0;
+  int i, res = a;
+
+  if (SGN(x) < 0) --l;
+  else ++l;
+  
+  for (i = 0; i < ABS(b); i++ {
+    res = res + l;
+  }
+
+  return res;
+}
