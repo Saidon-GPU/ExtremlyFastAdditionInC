4236388046613a33ae19a9a757d232e10615d422
Saidon-GPU <Primary.atias2@gmail.com>
2025-03-17 18:48:15 +0200

    Update fast_addition.c

--- fast_addition.c
+++ fast_addition.c
@@ -4,17 +4,15 @@
 #define SGN(x) ((x) < 0 ? -1 : 1)
 
 int fast_addition(int a, int b) {
-  int l = 0; // Variable to determine the increment/decrement step based on the sign of b.
-  int i, res = a; // Initialize 'res' to the value of 'a'. 'i' will be used as a loop counter.
+  int l = 0; 
+  int i, res = a;
 
-  // Determine the direction of addition/subtraction based on the sign of 'b'.
-  if (SGN(b) < 0) --l; // If 'b' is negative, set 'l' to -1 (decrement step).
-  else ++l; // Otherwise, set 'l' to +1 (increment step).
+  if (SGN(b) < 0) --l;
+  else ++l;
 
-  // Loop to add/subtract 'l' to/from 'res' a total of |b| times.
   for (i = 0; i < ABS(b); i++) {
-    res = res + l; // Increment or decrement 'res' by 'l' in each iteration.
+    res = res + l;
   }
 
-  return res; // Return the final result after completing the loop.
+  return res;
 }
